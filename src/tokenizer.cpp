4d07bd4d8833fec187827dd1a00cf2c83ac7fae8
Ian Macalinao <me@ian.pw>
2015-12-07 14:27:28 -0600

    string support

--- src/tokenizer.cpp
+++ src/tokenizer.cpp
@@ -7,12 +7,21 @@ std::vector<std::string>* tokenize(std::string str) {
   std::string curToken = "";
 
   bool proceed = false;
-  // bool inString = false;
+  bool inString = false;
 
   for (std::string::iterator it = str.begin(); it != str.end(); it++) {
     char c = *it;
 
+    if (inString && c != '"') {
+      curToken += c;
+      continue;
+    }
+
     switch (c) {
+
+      case '"':
+        inString = !inString;
+        break;
 
       case ' ':
         proceed = true;
@@ -26,7 +35,6 @@ std::vector<std::string>* tokenize(std::string str) {
 
     if (proceed) {
       tokens->push_back(curToken);
-      std::cout << curToken << std::endl;
       curToken = "";
       proceed = false;
     }
